41efbac6534ee526e5551091600b0841fcb7977a
__Facud462__ <facud462@gmail.com>
2024-01-16 16:26:42 -0300

    implementacion del mini framework mvc

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,26 @@
-<?php
-const BASE_URL = "http://localhost/gestion/";
-const HOST = "localhost";
-const USER = "root";
-const PASS = "";
-const DB = "eventos";
-const CHARSET = "charset=utf8";
-?>
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://localhost/tienda_virtual";
+
+	//Zona horaria
+	date_default_timezone_set('America/Guatemala');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "tienda";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "charset=utf8";
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "Q";
+
+	
+
+
+ ?>
--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -1,9 +1,10 @@
-<?php
-class HomeModel extends Query{
-    public function __construct()
-    {
-        parent::__construct();
-    }
-}
+<?php 
 
-?>
+	class homeModel extends Mysql
+	{
+		public function __construct()
+		{
+			parent::__construct();
+		}	
+	}
+ ?>
